8a62c215a02858b05223bdaf9b0772ed2b66e286
Kaustubh Singh <kaustubh.forge@gmail.com>
2025-08-07 13:36:31 -0500

    Added Caesar Ciper Project (Day 8)

---
+++ Caesar Cipher/Caesar_Cipher.py
@@ -0,0 +1,74 @@
+# Modules imported
+from art import logo
+
+# ASCII art for aesthetics
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# User inputs to start Caesar Cipher
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# User-defined function to shift message forward by a specific number
+def encrypt(original_text,shift_amount):
+    new_text=""
+
+    # Loop for iterating through message + conditional statements that cause forward letter shifts
+    for old_letter in original_text:
+        if old_letter not in alphabet: # Keeps symbols and their positions unchanged
+            new_text+=old_letter
+        else:
+            old_index = alphabet.index(old_letter) # Accessing index
+            new_index = old_index + shift_amount # Replaces old letter with a new letter after shift
+            new_index = new_index % len(alphabet)  # Modulo finds remainder for the new index
+            # If the new index precedes the number of indexes available for use(is out of range), we use the remainder to get the shift value without encountering the out of range error
+            # so, for example if new index is 27, but we only have 25 available, we get 2. So we can safely move from z(which is index 25) to b(which is index "27" even though it is really index 1)
+            # if our new index is 22, then nothing will happen because 22%25 is still 22.
+            new_text = new_text + alphabet[new_index]
+
+    print(f"The encoded result is {new_text}.")
+
+# User-defined function to shift message backwards by a specific number
+def decrypt(original_text,shift_amount):
+    new_text2=""
+    for old_letter in original_text:
+        if old_letter not in alphabet: # Keeps symbols and their positions unchanged
+            new_text2+=old_letter
+        else:
+            old_index = alphabet.index(old_letter) # Accessing index
+            new_index = old_index - shift_amount # Replaces old letter with a new letter after shift
+            new_index = new_index % len(alphabet) # Modulo finds the remainder of the new index
+            # If the new index precedes the number of indexes available for use(is out of range), we use the remainder to get the shift value without encountering the out of range error
+            # so, for example if new index is 27, but we only have 25 available, we get 2. So we can safely move from z(which is index 25) to b(which is index "27" even though it is really index 1)
+            # if our new index is 22, then nothing will happen because 22%25 is still 22.
+            new_text2 += alphabet[new_index]
+    print(f"The decoded result is {new_text2}.")
+
+# User-defined function that calls other functions for encoding and decoding
+def caesar(direction_to_go,original_text,shift_amount):
+    if direction_to_go=="encode":
+        encrypt(text,shift) # We can also use encrypt(original_text=text,shift_amount=shift)
+    elif direction_to_go=="decode":
+        decrypt(text,shift) # We can also use decrypt(original_text=text,shift_amount=shift)
+    else:
+        print("You entered an invalid option!")
+
+# Calls the caesar function
+caesar(direction,original_text=text,shift_amount=shift)
+
+# Loop to enable continuation of Caesar Cipher
+while True:
+    check_again = input("Type 'yes' if you want to go again. Otherwise, type 'no'\n").lower()
+    if check_again=="yes":
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        # Calls function
+        caesar(direction, original_text=text, shift_amount=shift)
+
+    else:
+        print("Well...I suppose we are done for the day.")
+        break # Breaks out of the while loop and prevents further runs
---
+++ Caesar Cipher/README.md
@@ -0,0 +1,57 @@
+# 🔡Caesar Cipher Project | Day 8- 100 Days of Python
+
+## A clever project that hides messages by shifting letters, like sending and receiving secret codes the ancient roman way📜
+
+### **What It Teaches**
+ - How to create `user-defined` functions
+ - How to call `user-defined` functions
+ - How to use `index()` to access the index of an element
+ - How to use `break` to break out of a loop
+ - How to use `%` to find the remainder
+
+### ❓**How Is It Run**
+
+1. Open a terminal  
+2. Navigate to the project folder:
+   ```bash
+   cd 100DaysOfPython_Kaustubh/"Caesar Cipher"
+   python Caesar_Cipher.py
+   ```
+3. You can also run the code in an IDLE such as Visual Studio Code or PyCharm
+
+### 💻 **Sample Output**
+```
+          
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88
+            88             88
+           ""             88
+                          88
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
+8b         88 88       d8 88       88 8PP" 88
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88
+              88
+              88
+
+Type 'encode' to encrypt, type 'decode' to decrypt:
+encode
+Type your message:
+Tom is here
+Type the shift number:
+2
+The encoded result is vqo ku jgtg.
+Type 'yes' if you want to go again. Otherwise, type 'no'
+```
+
+### 🙏 **Credits**
+This project is part of my journey through the 
+[100 Days of Code: Python Bootcamp](https://www.udemy.com/course/100-days-of-code/) by Dr. Angela Yu.  
+
+The project idea comes from the course, but the code and documentation here are my own work.  
+At times I referenced the course material when stuck, and in some cases my solution may look very similar to the course’s — simply because there are only a few straightforward ways to solve certain beginner problems.  
+Either way, the implementation reflects my own understanding and learning process.
